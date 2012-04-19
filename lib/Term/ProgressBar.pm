cd99d65a8cc30f723abdc66ca1ec8f34fd7e5e1d
Filip Sergot <filip@sergot.pl>
2012-04-20 01:42:44 +0200

    Time left printing

--- lib/Term/ProgressBar.pm
+++ lib/Term/ProgressBar.pm
@@ -12,19 +12,26 @@ has $.left = '['; # TODO: better names?
 has $.right = ']';
 has $.style = '=';
 
-has Str $!as; # current string
+has Str $!as = ""; # current string
+
+has $!step = 0.0;
 
 method update(Int $step) {
+	my $start = nqp::p6box_n(pir::time__n());
+
 	my $multi = ($step/($.count/$.width)).floor;
 	my $ext = ' ';
 
 	$ext ~= $multi*(100/$.width).round(0.1)~"%" if $.p;
+	$ext ~= ' '~ (( $start - $!step ) * ( $.count - $step ) ).floor ~ ' s' ;
 
 	if $step % ($.count/$.width).floor == 0 {
 		$!as = "$.name "~$.left~($.style x $multi)~(' ' x ($.width - $multi))~$.right~" $ext";
 		print $!as, "\r";
 	}
-	say if $step == $.count;
+	say '' if $step == $.count;
+
+	$!step = $start;
 }
 
 method message(Str $s) {
